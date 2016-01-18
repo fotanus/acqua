d7aa950b8e45c1a6bf91c0d57b14c4eb080938c6
Felipe Tanus <fotanus@gmail.com>
2016-01-18 16:14:58 -0200

    Rename L1c to IR

--- src/IR.hs
+++ src/IR.hs
@@ -1,7 +1,5 @@
 module Main where
 
-import Text.Show.Pretty
-
 import L1.Grammar
 import AcquaIR.Language
 import AcquaIR.Compile
@@ -12,9 +10,6 @@ main = do
   let eitherAst = parse s
   case eitherAst of
     Right ast -> do
-      putStrLn "===Ast==="
-      putStrLn (ppShow ast)
-      putStrLn "===IR==="
       putStrLn (printProgram (compile ast))
     Left errorMsg ->
       putStrLn errorMsg
