15bb63245908d6bb1600269c053a59609c998049
fotanus@gmail.com <Felipe Tanus>
2016-01-19 00:08:21 -0200

    Return

--- src/Simulator/Rules/All.hs
+++ src/Simulator/Rules/All.hs
@@ -6,8 +6,8 @@ import Simulator.Rules
 import Simulator.Rules.AssignJob
 import Simulator.Rules.Call
 import Simulator.Rules.NewEnv
-
 import Simulator.Rules.Assign
+import Simulator.Rules.Return
 
 rules :: [Rule]
-rules = [assignJob, assignV, assignL, assignI, envNew, call]
+rules = [assignJob, assignV, assignL, assignI, envNew, call, returnTerminator]
---
+++ src/Simulator/Rules/Return.hs
@@ -0,0 +1,41 @@
+module Simulator.Rules.Return where
+
+import qualified Data.Map as Map
+import Debug.Trace
+
+import AcquaIR.Language as IR
+import Simulator.Acqua
+import Simulator.ProcessingUnit as PU
+import Simulator.Interconnection
+import Simulator.Environment
+
+import Simulator.Rules
+
+returnTerminator :: Rule
+returnTerminator (Acqua bb q pus i f) =
+    Acqua bb q pus' i' f
+  where
+    (i', pus') = stepReturn i pus
+
+stepReturn :: Interconnection -> [ProcessingUnit] -> (Interconnection, [ProcessingUnit])
+stepReturn i [] = (i,[])
+stepReturn i (pu:pus) =
+  case (PU.commands pu,PU.terminator pu) of
+    ([], Return x) -> trace "return" (i'', pu':pus')
+      where
+        (i'', pus') = stepReturn i' pus
+        -- interconnection
+        Just cenv = Map.lookup ce rEnv
+        Just returnValue = Map.lookup x cenv
+        Just (ReturnAddr pId' envId' x') = Map.lookup ce ra
+        m = Message pId' envId' x' returnValue
+        i' = m : i
+
+        -- pu
+        PU pId _ _ ce rEnv cEnv ra cc se = pu
+        cc' = Map.insert ce 0 cc
+        pu' = PU pId [] Empty ce rEnv cEnv ra cc' se
+
+    _ -> (i', pu:pus')
+      where (i', pus') = stepReturn i pus
+
