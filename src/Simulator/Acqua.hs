5672d96c5404ff4d81523ed1f1befed4083d2a99
Felipe Tanus <fotanus@gmail.com>
2016-01-20 17:21:10 -0200

    Method to count rules usage

--- src/Simulator/Acqua.hs
+++ src/Simulator/Acqua.hs
@@ -10,13 +10,30 @@ import Simulator.Queue
 type FinishFlag = Bool
 data StateValue = IntVal Int
                 | NewEnvIds (Map.Map (Int,String) String)
+                | RulesUsed (Map.Map String Int)
                 deriving (Eq,Show)
 
-data Acqua = Acqua Program Queue [ProcessingUnit] Interconnection FinishFlag (Map.Map String StateValue)
+type AcquaState = Map.Map String StateValue
+
+statesDefault :: AcquaState
+statesDefault = Map.fromList [
+    ("newEnvIds", (NewEnvIds (Map.fromList []))),
+    ("rulesUsed", (RulesUsed (Map.fromList []))),
+    ("envId",IntVal 0)
+  ]
+
+countRule :: AcquaState-> String -> AcquaState
+countRule s rule = Map.insert rule (RulesUsed newRulesUsed) s
+  where
+    Just (RulesUsed rulesUsed) = Map.lookup "rulesUsed" s
+    Just count = Map.lookup rule rulesUsed
+    newRulesUsed = Map.insert rule (count + 1) rulesUsed
+
+data Acqua = Acqua Program Queue [ProcessingUnit] Interconnection FinishFlag AcquaState
   deriving (Show,Eq)
 
 newAcqua :: Program -> Int -> Acqua
-newAcqua p n = Acqua p queue processingUnits newInterconnection False (Map.fromList [("newEnvIds", (NewEnvIds (Map.fromList []))), ("envId",IntVal 0)])
+newAcqua p n = Acqua p queue processingUnits newInterconnection False statesDefault
   where
     processingUnits = newProcessingUnits n
     specialProcessingUnit = head processingUnits
