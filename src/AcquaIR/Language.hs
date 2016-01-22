e14662a666971981091d44a95adb4f9fef43a9c0
Felipe Tanus <fotanus@gmail.com>
2016-01-22 12:25:59 -0200

    tweak ir dump with some colors

--- src/AcquaIR/Language.hs
+++ src/AcquaIR/Language.hs
@@ -52,11 +52,14 @@ printProgram p = foldr (++) "" (map printBasicBlock p)
 ident :: String
 ident = "    "
 
-startRed :: String
-startRed = "\x1b[31m"
+blue :: String
+blue = "\x1b[34m"
 
-startGreen :: String
-startGreen = "\x1b[32m"
+yellow :: String
+yellow = "\x1b[33m"
+
+green :: String
+green = "\x1b[32m"
 
 noColor :: String
 noColor = "\x1b[0m"
@@ -65,27 +68,27 @@ printBasicBlock :: BasicBlock -> String
 printBasicBlock (BB l _ cs t) = (printLabel l) ++ (printCommands cs) ++ (printTerminator t)
 
 printLabel :: Label -> String
-printLabel l = l ++ ":\n"
+printLabel l = yellow ++ l ++ noColor ++ ":\n"
 
 printCommands :: [Command] -> String
 printCommands cs = foldr (++) "" (map printCommand cs)
 
 printCommand :: Command -> String
-printCommand (EnvNew i n) = ident ++ i ++ " = EnvNew " ++ (show n) ++ "\n"
-printCommand (EnvAddI i name n) = ident ++ "EnvAdd " ++ i ++ " " ++ name ++ " " ++ (show n) ++ "\n"
-printCommand (EnvAddL i name l) = ident ++ "EnvAdd " ++ i ++ " " ++ name ++ " " ++ l ++ "\n"
-printCommand (Call name1 name2 i) = ident ++ name1 ++ " = Call " ++ name2 ++ " " ++ i ++ "\n"
+printCommand (EnvNew i n) = ident ++ blue ++ i ++ " = EnvNew " ++ (show n) ++ noColor ++ "\n"
+printCommand (EnvAddI i name n) = ident ++ blue ++ "EnvAdd " ++ i ++ " " ++ name ++ " " ++ (show n) ++ noColor ++ "\n"
+printCommand (EnvAddL i name l) = ident ++ blue ++ "EnvAdd " ++ i ++ " " ++ name ++ " " ++ l ++ noColor ++ "\n"
+printCommand (Call name1 name2 i) = ident ++ blue ++ name1 ++ " = Call " ++ name2 ++ " " ++ i ++ noColor ++ "\n"
 printCommand (Op name1 op name2) = ident ++ "resp = " ++ name1 ++ " " ++ (printOpCode op) ++ " " ++ name2 ++ "\n"
 printCommand (AssignI name n) = ident ++ name ++ " = " ++ (show n) ++ "\n"
 printCommand (AssignL name l) = ident ++ name ++ " = \"" ++ l ++ "\"\n"
 printCommand (AssignV name1 name2) = ident ++ name1 ++ " = " ++ name2 ++ "\n"
-printCommand (Wait) = ident ++ "Wait" ++ "\n"
+printCommand (Wait) = ident ++ blue ++ "Wait" ++ noColor ++ "\n"
 
 printTerminator :: Terminator -> String
-printTerminator (Goto l) = ident ++ "goto " ++ l ++ "\n"
-printTerminator (Return name) = ident ++ "return " ++ name ++ "\n"
-printTerminator (If name l) = ident ++ "if " ++ name ++ " goto " ++ l ++ "\n"
-printTerminator Empty = ident ++ "empty\n" 
+printTerminator (Goto l) = ident ++ green ++ "goto " ++ l ++ noColor ++ "\n"
+printTerminator (Return name) = ident ++ green ++ "return " ++ name ++ noColor ++ "\n"
+printTerminator (If name l) = ident ++ green ++ "if " ++ name ++ " goto " ++ l ++ noColor ++ "\n"
+printTerminator Empty = ident ++ "empty\n"
 
 printOpCode :: OpCode -> String
 printOpCode And = "&&"
