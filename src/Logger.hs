bd94685077baf4c55f48d15f887b67117d727528
Felipe Tanus <fotanus@gmail.com>
2016-02-22 14:16:59 -0300

    option to remove traces

--- src/Logger.hs
+++ src/Logger.hs
@@ -2,8 +2,16 @@ module Logger where
 
 import Debug.Trace
 
+doTrace :: Bool
+doTrace = True
+
 trace :: String -> a -> a
-trace s x = Debug.Trace.trace s x
+trace msg ret = if doTrace
+              then Debug.Trace.trace msg ret
+              else ret
 
 traceShow :: Show a => a -> b -> b
-traceShow x = Debug.Trace.traceShow x
+traceShow msg ret = if doTrace
+                then Debug.Trace.traceShow msg ret
+                else ret
+
