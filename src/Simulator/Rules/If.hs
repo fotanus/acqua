f688dc9d261494e92a2686ca97b7f3c46428991f
fotanus@gmail.com <Felipe Tanus>
2016-07-01 17:22:00 -0300

    Fix if debug message

--- src/Simulator/Rules/If.hs
+++ src/Simulator/Rules/If.hs
@@ -17,7 +17,7 @@ ifRule acqua = acqua { processingUnits = newPus }
     bb = program acqua
     executeIf pu =
       case (PU.commands pu,PU.terminator pu, PU.canExecuteCmds pu) of
-        ([], If x l, True) -> trace ((show (PU.puId pu))++": " ++ (show (currentEnv pu)) ++ "if " ++ x ++ " goto " ++ l ++ "( x = " ++ (show val) ++ ")") pu'
+        ([], If x l, True) -> trace ((show (PU.puId pu))++": " ++  "if " ++ x ++ " goto " ++ l ++ " (x = " ++ (show val) ++ ")") pu'
          where
            ce = currentEnv pu
            env = environments pu
