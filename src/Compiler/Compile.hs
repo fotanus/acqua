bc6589a06592ee3196f7b90648e3391ce58aa49f
Felipe Tanus <fotanus@gmail.com>
2016-08-22 15:41:01 -0300

    remove tail code from compilation

--- src/Compiler/Compile.hs
+++ src/Compiler/Compile.hs
@@ -19,27 +19,27 @@ compile :: L1.Term -> IR.Program
 compile t =
   addWaits (eliminateRedundantVars (statementsToProgram statements))
   where
-    (c, bb) = evalState (_compile (fillFreeVars t)) defaultCompileStates
-    statements = [SL "main"] ++ c ++ [ST (Return resp)] ++ bb
+    c = evalState (_compile (fillFreeVars t)) defaultCompileStates
+    statements = [SL "main"] ++ c ++ [ST (Return resp)]
 
 
-_compile :: L1.Term -> State CompileStates ([Statement], [Statement])
-_compile (Num n)   = return ([SC (AssignI resp n)],[])
+_compile :: L1.Term -> State CompileStates [Statement]
+_compile (Num n)   = return [SC (AssignI resp n)]
 _compile (Ident n) = do
     c <- getCallRecordInfo n
     return $ case c of
-               Nothing -> ([SC (AssignV resp n)],[])
-               Just (fn,_,params,vars,False) -> ([
+               Nothing -> [SC (AssignV resp n)]
+               Just (fn,_,params,vars,False) -> [
                            SC (NewCallRecord "callRecord" ((length vars)+(length params))),
                            SC (SetCallRecordFn "callRecord" fn)
                          ] ++ freeVarsSetParams ++ [
                            SC (SetCallRecordMissingI "callRecord" (length params)),
                            SC (SetCallRecordCountI "callRecord" ((length vars))),
                            SC (AssignV resp "callRecord")
-                         ],[])
+                         ]
                         where
                           freeVarsSetParams = map (\(v,idx) -> SC (SetCallRecordParamI "callRecord" idx v)) (zip vars [0..])
-               Just (fn,_,params,vars,True) -> ([
+               Just (fn,_,params,vars,True) -> [
                            SC (NewCallRecord "callRecord" ((length vars')+(length params)+1)),
                            SC (SetCallRecordFn "callRecord" fn),
                            SC (SetCallRecordParamIL "callRecord" 0 fn)
@@ -47,38 +47,48 @@ _compile (Ident n) = do
                            SC (SetCallRecordMissingI "callRecord" 1),
                            SC (SetCallRecordCountI "callRecord" ((length vars')+(length params))),
                            SC (AssignV resp "callRecord")
-                         ],[])
+                         ]
                         where
                           vars' = if null vars then [] else tail vars
                           freeVarsSetParams = map (\(v,idx) -> SC (SetCallRecordParamI "callRecord" idx v)) (zip vars [1..])
 
 _compile (Fn params t1 freeVars) = do
   fn <- nextFnLabel
-  (c1,bb1) <- _compile t1
+  c1 <- _compile t1
   freeVarsSetParams <- return $ map (\(v,idx) -> SC (SetCallRecordParamI "callRecord" idx v)) (zip freeVars [0..])
   getParamsCommands <- return $ map (\p-> SC (GetCallRecordParam "callRecord" (snd p) (fst p))) (zip freeVars [0..])
   getParamsCommands' <- return $ getParamsCommands ++ (map (\p-> SC (GetCallRecordParam "callRecord" (snd p) (fst p))) (zip params [(length getParamsCommands)..] ))
+  fnBody <- return $ [SL fn] ++ getParamsCommands' ++ c1 ++ [ST (Return resp)]
+  continueLabel <- nextContinueLabel
   newCallRecordCommands <- return $ [
                                    SC (NewCallRecord "callRecord" ((length freeVars)+(length params))),
                                    SC (SetCallRecordFn "callRecord" fn)
                                  ] ++ freeVarsSetParams ++ [
                                    SC (SetCallRecordMissingI "callRecord" (length params)),
                                    SC (SetCallRecordCountI "callRecord" (length freeVars)),
-                                   SC (AssignV resp "callRecord")
+                                   SC (AssignV resp "callRecord"),
+                                   ST (Goto  continueLabel)
+                                 ] ++ fnBody ++ [
+                                  SL continueLabel
                                  ]
-  fnBody <- return $ [SL fn] ++ getParamsCommands' ++ c1 ++ [ST (Return resp)]
-  return (newCallRecordCommands, fnBody++ bb1)
+  return newCallRecordCommands
 
 
 _compile (App t1 t2) = do
-  (c1,bb1) <- _compile t1
-  (c2,bb2) <- _compile t2
+  c1 <- _compile t1
+  c2 <- _compile t2
   thenLabel <- nextThenLabel
   backLabel <- nextBackLabel
   dummyLabel <- nextDummyLabel
   callRecordIdent <- nextIdentName
   callRecordIdent' <- nextIdentName
-  envs <- return  [
+  bbThen <- return $ [
+                       SL thenLabel,
+                       SC (AssignV callRecordIdent callRecordIdent'),
+                       SC (Call "resp" callRecordIdent),
+                       ST (Goto backLabel)
+                     ]
+  envs <- return $ [
                     SC (AssignV "param" resp),
                     SC (AssignI "one" 1),
                     SC (AssignV callRecordIdent' callRecordIdent'),
@@ -95,70 +105,82 @@ _compile (App t1 t2) = do
                     ST (IR.If resp thenLabel),
                     SL dummyLabel,
                     SC (AssignV "resp" callRecordIdent'),
-                    ST (Goto backLabel),
+                    ST (Goto backLabel)
+                  ] ++ bbThen ++ [
                     SL backLabel
                   ]
-  bbThen <- return $ [
-                       SL thenLabel,
-                       SC ( AssignV callRecordIdent callRecordIdent'),
-                       SC (Call "resp" callRecordIdent),
-                       ST (Goto backLabel)
-                     ]
   cs <- return $ c1 ++ [SC (AssignV callRecordIdent' resp)] ++ c2 ++ envs
-  return (cs, bbThen ++ bb1 ++ bb2)
+  return cs
 
 _compile (L1.Op t1 op t2) = do
-  (c1,bb1) <- _compile t1
-  (c2,bb2) <- _compile t2
+  c1 <- _compile t1
+  c2 <- _compile t2
   t1Ident <- nextIdentName
   t2Ident <- nextIdentName
   t1c <- return $ c1 ++ [SC (AssignV t1Ident resp)]
   t2c <- return $ c2 ++ [SC (AssignV t2Ident resp)]
   cs <- return $ t1c ++ t2c ++ [SC (IR.Op "resp" t1Ident (setOp op) t2Ident)]
-  return (cs, bb1 ++ bb2)
+  return cs
 
 _compile (L1.If t1 t2 t3) = do
-  (c1,bb1) <- _compile t1
-  (c2,bb2) <- _compile t2
-  (c3,bb3) <- _compile t3
+  c1 <- _compile t1
+  c2 <- _compile t2
+  c3 <- _compile t3
   thenLabel <- nextThenLabel
   backLabel <- nextBackLabel
   dummyLabel <- nextDummyLabel
+  continueLabel <- nextContinueLabel
   t1c <- return $ c1 ++ [ST (IR.If resp thenLabel)] ++ [SL dummyLabel]
   t3c <- return $ c3 ++ [ST (Goto backLabel)] ++ [SL backLabel]
   bbThen <- return $ [SL thenLabel] ++ c2 ++ [ST (Goto backLabel)]
-  cs <- return $ t1c ++ t3c
-  bbs <- return $ bbThen ++ bb1 ++ bb2 ++ bb3
-  return (cs, bbs)
+  cs <- return $ t1c ++ t3c ++ [ST (Goto continueLabel)] ++ bbThen ++ [(SL continueLabel)]
+  return cs
 
-_compile (Let n (Fn params t1 freevars) t2) = do
-  (c1,bb1) <- _compile (Fn params t1 freevars)
+_compile (Let n (Fn params t1 freeVars) t2) = do
   _ <- addKnownVars n
-  let SL fn = head bb1
-  _ <- setCallRecordInfo n (fn, n, params, freevars, False)
-  (c2,bb2) <- _compile t2
-  cs <- return $  c1 ++ [SC (AssignV n resp)] ++ c2
-  return (cs, bb1 ++ bb2)
+  fn <- nextFnLabel
+  c1 <- _compile t1
+  freeVarsSetParams <- return $ map (\(v,idx) -> SC (SetCallRecordParamI "callRecord" idx v)) (zip freeVars [0..])
+  getParamsCommands <- return $ map (\p-> SC (GetCallRecordParam "callRecord" (snd p) (fst p))) (zip freeVars [0..])
+  getParamsCommands' <- return $ getParamsCommands ++ (map (\p-> SC (GetCallRecordParam "callRecord" (snd p) (fst p))) (zip params [(length getParamsCommands)..] ))
+  fnBody <- return $ [SL fn] ++ getParamsCommands' ++ c1 ++ [ST (Return resp)]
+  continueLabel <- nextContinueLabel
+  newCallRecordCommands <- return $ [
+                                   SC (NewCallRecord "callRecord" ((length freeVars)+(length params))),
+                                   SC (SetCallRecordFn "callRecord" fn)
+                                 ] ++ freeVarsSetParams ++ [
+                                   SC (SetCallRecordMissingI "callRecord" (length params)),
+                                   SC (SetCallRecordCountI "callRecord" (length freeVars)),
+                                   SC (AssignV resp "callRecord"),
+                                   ST (Goto  continueLabel)
+                                 ] ++ fnBody ++ [
+                                  SL continueLabel
+                                 ]
+  _ <- setCallRecordInfo n (fn, n, params, freeVars, False)
+  c2 <- _compile t2
+  cs <- return $  newCallRecordCommands ++ [SC (AssignV n resp)] ++ c2
+  return cs
 
 _compile (Let n t1 t2) = do
   _ <- addKnownVars n
-  (c1,bb1) <- _compile t1
-  (c2,bb2) <- _compile t2
+  c1 <- _compile t1
+  c2 <- _compile t2
   cs <- return $  c1 ++ [SC (AssignV n resp)] ++ c2
-  return (cs, bb1 ++ bb2)
+  return cs
 
 _compile (Letrec n (Fn params t1 freeVars) t2) = do
   _ <- addKnownVars n
   freeVars' <- return $ delete n freeVars
   fn <- nextFnLabel
   _ <- setCallRecordInfo n (fn, n, params, freeVars', True)
-  (c1,bb1) <- _compile t1
+  continueLabel <- nextContinueLabel
+  c1 <- _compile t1
   getParamsCommands <- return $ [SC (GetCallRecordParam "callRecord" 0 n)]
   getParamsCommands' <- return $ getParamsCommands ++ map (\p-> SC (GetCallRecordParam "callRecord" (snd p) (fst p))) (zip freeVars' [1..])
   getParamsCommands'' <- return $ getParamsCommands' ++ (map (\p-> SC (GetCallRecordParam "callRecord" (snd p) (fst p))) (zip params [(length getParamsCommands')..] ))
   fnBody <- return $ [SL fn] ++ getParamsCommands'' ++ c1 ++ [ST (Return resp)]
-  (c2,bb2) <- _compile t2
-  return (c2, fnBody ++ bb1 ++ bb2)
+  c2 <- _compile t2
+  return $ c2 ++ [ST (Goto continueLabel)] ++ fnBody ++ [(SL continueLabel)]
 
 _compile (Letrec _ _ _) = error "Letrec first term should be a function"
 
--- src/Compiler/CompileStates.hs
+++ src/Compiler/CompileStates.hs
@@ -15,47 +15,59 @@ data CompileStates = CompileStates {
   identNum :: Int,
   callRecordInfo :: [(String,CallRecordInfoT)],
   fnLabelNames :: [(String,String)],
-  knownVarsList :: [String]
+  knownVarsList :: [String],
+  continueLabelNum :: Int
   }
 
 defaultCompileStates :: CompileStates
-defaultCompileStates = (CompileStates 0 0 0 0 0 [] [] [])
+defaultCompileStates = (CompileStates 0 0 0 0 0 [] [] [] 0)
+
+nextContinueLabel :: State CompileStates Label
+nextContinueLabel = do
+  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs continueN <- get
+  put (CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs (continueN + 1))
+  return $ "continue" ++ (show continueN)
 
 nextThenLabel :: State CompileStates Label
 nextThenLabel = do
-  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs <- get
-  put (CompileStates (thenN+1) backN dummyN fnN identN closInfo fnLabels kvs)
+  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs continueN <- get
+  put (CompileStates (thenN+1) backN dummyN fnN identN closInfo fnLabels kvs continueN)
   return $ "then" ++ (show thenN)
 
 nextBackLabel :: State CompileStates Label
 nextBackLabel = do
-  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs <- get
-  put (CompileStates thenN (backN+1) dummyN fnN identN closInfo fnLabels kvs)
+  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs continueN <- get
+  put (CompileStates thenN (backN+1) dummyN fnN identN closInfo fnLabels kvs continueN)
   return $ "back" ++ (show backN)
 
 nextDummyLabel :: State CompileStates Label
 nextDummyLabel = do
-  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs <- get
-  put (CompileStates thenN backN (dummyN+1) fnN identN closInfo fnLabels kvs)
+  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs continueN <- get
+  put (CompileStates thenN backN (dummyN+1) fnN identN closInfo fnLabels kvs continueN)
   return $ "dummy" ++ (show dummyN)
+
+lastFnLabel :: State CompileStates Label
+lastFnLabel = do
+  CompileStates _ _ _ fnN _ _ _ _ _ <- get
+  return $ "_fn_" ++ (show (fnN-1))
 
 nextFnLabel :: State CompileStates Label
 nextFnLabel = do
-  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs <- get
-  put (CompileStates thenN backN dummyN (fnN+1) identN closInfo fnLabels kvs)
+  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs continueN <- get
+  put (CompileStates thenN backN dummyN (fnN+1) identN closInfo fnLabels kvs continueN)
   return $ "_fn_" ++ (show fnN)
 
 nextIdentName :: State CompileStates IR.Name
 nextIdentName = do
-  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs <- get
-  put (CompileStates thenN backN dummyN fnN (identN+1) closInfo fnLabels kvs)
+  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs continueN <- get
+  put (CompileStates thenN backN dummyN fnN (identN+1) closInfo fnLabels kvs continueN)
   return $ "var" ++ (show identN)
 
 setCallRecordInfo :: String -> CallRecordInfoT -> State CompileStates IR.Name
 setCallRecordInfo a b = do
-  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs <- get
+  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs continueN <- get
   closInfo' <- return $ (a,b) : (closInfo)
-  put (CompileStates thenN backN dummyN fnN identN closInfo' fnLabels kvs)
+  put (CompileStates thenN backN dummyN fnN identN closInfo' fnLabels kvs continueN)
   return $ "var" ++ (show identN)
 
 getCallRecordInfo :: String -> State CompileStates (Maybe CallRecordInfoT)
@@ -65,16 +77,16 @@ getCallRecordInfo a = do
 
 setFnLabelName :: String -> String -> State CompileStates IR.Name
 setFnLabelName a b = do
-  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs <- get
+  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs continueN <- get
   fnLabels' <- return $ (a,b) : fnLabels
-  put (CompileStates thenN backN dummyN fnN identN closInfo fnLabels' kvs)
+  put (CompileStates thenN backN dummyN fnN identN closInfo fnLabels' kvs continueN)
   return $ "var" ++ (show identN)
 
 addKnownVars :: String -> State CompileStates IR.Name
 addKnownVars a = do
-  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs <- get
+  CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs continueN <- get
   kvs' <- return $ a : kvs
-  put (CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs')
+  put (CompileStates thenN backN dummyN fnN identN closInfo fnLabels kvs' continueN)
   return $ "var" ++ (show identN)
 
 getKnownVars :: State CompileStates [String]
